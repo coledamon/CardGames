eda09a1d3867f8cf8ade360b66e9c7cef5f3ce11
sportsziggy <sportsziggy@aol.com>
2016-09-21 13:54:06 -0400

    updated Card before windows update

--- CardGames/Card.cs
+++ CardGames/Card.cs
@@ -5,6 +5,48 @@ namespace CardGames
 {
     public class Card
     {
+        Suit cardSuit = new Suit();
+        Rank cardRank = new Rank();
 
+        public Card(Suit suit, Rank rank)
+        {
+            cardSuit = suit;
+            cardRank = rank;
+        }
+
+        public int CompareTo(Card OtherCardObject)
+        {
+            if (this.rank == OtherRankObject.rank && this.cardSuit == OtherCardObject.suit)
+            {
+                return 0;
+            }
+            else if(this.cardRank == OtherCardObject.cardRank && this.cardSuit < OtherCardObject.cardSuit)
+            {
+                return -1;
+            }
+            else if (this.rank < OtherRankObject.rank)
+            {
+                return -1;
+            }
+            else
+            {
+                return 1;
+            }
+        }
+
+        public Rank GetRank()
+        {
+
+        }
+
+        public Suit GetSuit()
+        {
+
+        }
+
+        public String ToString()
+        {
+
+        }
     }
 }
