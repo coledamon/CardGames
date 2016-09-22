da9ea29e2635e299fa0da17763879a54d4e5f07c
sportsziggy <sportsziggy@aol.com>
2016-09-22 14:27:35 -0400

    added hand

--- CardGames/Deck.cs
+++ CardGames/Deck.cs
@@ -1,59 +1,61 @@
 ﻿using System;
 using System.Collections.Generic;
 
-public class Deck
+namespace CardGames
 {
-    List<Card> usedDeck = new List<Card>();
-
-	public Deck()
-	{
-        List<Card> deckOfCards = new List<Card>();
-	}
-
-    public void AddCard(Card card)
+    public class Deck
     {
-        deckOfCards.Add(card);
-    }
-    //removed () from first
-    public Card DealOne()
-    {
-        usedDeck.Add(deckOfCards.First);
-        deckOfCards.RemoveAt(0);
-        return deckOfCards.First;
-    }
+        List<Card> usedDeck = new List<Card>();
 
-    public int GetDeckSize()
-    {
-        return deckOfCards.Count;
-    }
-
-    public bool isEmpty()
-    {
-        if (deckOfCards.Count == 0)
-            return true;
-        else
-            return false;
-    }
-
-    public void Shuffle()
-    {
-        //var rng = new Random();
-        //List<Card> shuffledDeckOfCards = deckOfCards.OrderBy(item => rng.Next());
-        int n = deckOfCards.Count;
-        Random rng = new Random();
-        while (n > 1)
+        public Deck()
         {
-            int j = (rng.Next(0, n) % n);
-            n--;
-            Card value = deckOfCards[j];
-            deckOfCards[j] = deckOfCards[n];
-            deckOfCards[n] = value;
+            List<Card> deckOfCards = new List<Card>();
+        }
+
+        public void AddCard(Card card)
+        {
+            deckOfCards.Add(card);
+        }
+        //removed () from first
+        public Card DealOne()
+        {
+            usedDeck.Add(deckOfCards.First);
+            deckOfCards.RemoveAt(0);
+            return deckOfCards.First;
+        }
+
+        public int GetDeckSize()
+        {
+            return deckOfCards.Count;
+        }
+
+        public bool isEmpty()
+        {
+            if (deckOfCards.Count == 0)
+                return true;
+            else
+                return false;
+        }
+
+        public void Shuffle()
+        {
+            //var rng = new Random();
+            //List<Card> shuffledDeckOfCards = deckOfCards.OrderBy(item => rng.Next());
+            int n = deckOfCards.Count;
+            Random rng = new Random();
+            while (n > 1)
+            {
+                int j = (rng.Next(0, n) % n);
+                n--;
+                Card value = deckOfCards[j];
+                deckOfCards[j] = deckOfCards[n];
+                deckOfCards[n] = value;
+            }
+        }
+
+        public void RestoreDeck()
+        {
+            deckOfCards = deckOfCards.Concat(usedDeck);
         }
     }
-
-    public void RestoreDeck()
-    {
-        deckOfCards = deckOfCards.Concat(usedDeck);
-    }
-
 }
---
+++ CardGames/Hand.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CardGames
+{
+    public abstract class Hand
+    {
+        List<Hand> cardsInHand = new List<Hand>();
+        List<Hand> usedCards = new List<Hand>();
+
+        public void AddCard(Card card)
+        {
+            card.DealOne();
+            usedCards.Add(card.DealOne());
+        }
+
+        public abstract int CompareTo(Hand OtherHandObject)
+        {
+            if (this == OtherHandObject)
+            {
+                return 0;
+            }
+            else if (this < OtherRankObject)
+            {
+                return -1;
+            }
+            else
+            {
+                return 1;
+            }
+        }
+
+    }
+}
